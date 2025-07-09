af2f0f0a3f392690f7be74dcde6af96b507f918b
Abdul <abdulhakim54362@gmail.com>
2025-07-09 10:36:45 +0700

    Update: Navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,31 +1,46 @@
 <nav class="navbar px-4 py-3 position-relative align-items-center">
-    <div class="logo text-white fs-4 position-relative z-1">
-        <img src="{{ asset('images/logo.png') }}" alt="Logo">
+    <div class="logo fs-4 position-relative z-1 d-flex">
+        <div class="img">
+            <img class="w-75" src="{{ asset('images/logo.png') }}" alt="Logo">
+        </div>
+        <div class="logo-text">
+            <h5 class="nav-text fw-bold mb-0 ms-auto">Pelayanan NCAGE</h5>
+            <p class="nav-text fs-6 mb-0 ms-auto">Pusat Kodifikasi</p>
+        </div>
     </div>
     <div class="position-absolute top-50 start-50 translate-middle">
         <ul class="d-flex list-unstyled gap-4 mb-0">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Pendaftaran NCAGE</a></li>
-            <li><a href="#">Pantau Status</a></li>
+            <li><a class="btn fw-bold fs-7 {{ Request::is('/') ? 'text-white bg-active' : '' }}" href="#">Beranda</a></li>
+            <li><a class="btn rounded-4 fw-bold fs-7 {{ Request::is('pendaftaran-ncage') ? 'text-white bg-active' : '' }}" href="{{ route('pendaftaran-ncage.show', ['step' => 1]) }}">Pendaftaran NCAGE</a></li>
+            <li><a class="btn fw-bold fs-7" href="#">Pantau Status</a></li>
         </ul>
     </div>
+    @guest    
     <div class="ms-auto text-white position-relative z-1">
-        <div class="btn d-flex gap-3">
-            <a href="{{ route('login') }}">Login</a>
-            <a href="{{ route('register') }}">Register</a>
+        <div class="btn d-flex gap-1">
+            <a class="btn bg-white nav-text border border-2 border-active rounded-pill px-4 py-2" href="{{ route('login') }}">Masuk</a>
+            <a class="btn bg-active text-white rounded-pill px-4 py-2" href="{{ route('register') }}">Daftar</a>
         </div>
     </div>
+    @endguest
 
     @auth
+        <div class="ms-auto text-white position-relative z-1 dropdown">
             {{-- TAMPILKAN INI HANYA JIKA PENGGUNA SUDAH LOGIN --}}
+            <div class="profile-image">
+                <i class="fa-solid fa-circle-user"></i>
+            </div>
             
             {{-- Tombol Logout harus menggunakan form dengan method POST untuk keamanan --}}
-            <form method="POST" action="{{ route('logout') }}">
+            <div class="dropdown-menu">
+                <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <a href="{{ route('logout') }}" 
-                   onclick="event.preventDefault(); this.closest('form').submit();">
+                <a class="dropdown-item" href="{{ route('logout') }}" 
+                    onclick="event.preventDefault(); this.closest('form').submit();">
                     Logout
                 </a>
             </form>
-        @endauth
+            </div>
+        </div>
+    @endauth
 </nav>
